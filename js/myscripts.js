40ad11b9a5de13e27fb47e108ad2b02e139edfab
Nikola Jovic <nikjovke1@gmail.com>
2018-03-15 02:54:31 +0100

    Main navigation update

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,4 +1,19 @@
 $(document).ready(function()
 {
-    
-}
+    var main_navigation = $("#main-navigation");
+    var main_nav = main_navigation.find("#main-nav");
+
+    $("#navbar-toggler").click(function()
+    {
+        if (main_nav.hasClass("hide"))
+        {
+            main_nav.removeClass("hide");
+        }
+        else
+        {
+            main_nav.addClass("hide");
+        }
+
+        return false;
+    });
+});
