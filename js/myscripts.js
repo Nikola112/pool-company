7fb6f3f8c298ebe64679ff6acf0c12700d549822
Nikola Jovic <nikjovke1@gmail.com>
2018-03-16 00:46:56 +0100

    Main added

--- js/myscripts.js
+++ js/myscripts.js
@@ -3,6 +3,44 @@ $(document).ready(function()
     var main_navigation = $("#main-navigation");
     var main_nav = main_navigation.find("#main-nav");
 
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 50;
+    var navbarHeight = main_navigation.outerHeight();
+
+    $(window).scroll(function(event)
+    {
+        didScroll = true;
+    });
+
+    setInterval(function()
+    {
+        if (didScroll && $(window).width() < 991.98)
+        {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled()
+    {
+        var st = $(this).scrollTop();
+
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        if (st > lastScrollTop && st > navbarHeight)
+        {
+            main_navigation.addClass('nav-up');
+        }
+        else if(st + $(window).height() < $(document).height())
+        {
+            main_navigation.removeClass('nav-up');
+        }
+        
+        lastScrollTop = st;
+    }
+
     $("#navbar-toggler").click(function()
     {
         if (main_nav.hasClass("hide"))
